80626fb6ae8ba04dae5926a99b1c520cf5d1c8a1
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2024-04-02 21:01:39 +0545

    route update, view update

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
-
-
+Route::get('/signup', function () {
+    return view('register');
+});
+Route::get('/login', function () {
+    return view('login');
+});
 Route::post('/store', function (Request $req) {
     // Check the validation
     $validData = $req->validate([
